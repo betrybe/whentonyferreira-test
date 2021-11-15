57f6548a003ad59ef57435552ac25a4e4918e130
whentony <whentony.ferreira@gmail.com>
2021-11-15 12:43:39 -0300

    Inserindo itens na tabela

--- src/components/bar.js
+++ src/components/bar.js
@@ -5,18 +5,19 @@ import styles from '../css/bar.module.css';
 import consultExpense from '../service/service';
 
 const Bar = () => {
-  const [valor, setValor] = useState();
+  const [valor, setValor] = useState(0);
   const [descricao, setDescricao] = useState('');
-  const [moeda, setMoeda] = useState();
-  const [metodo, setMetodo] = useState();
-  const [tag, setTag] = useState();
+  const [moeda, setMoeda] = useState('USD');
+  const [metodo, setMetodo] = useState('dinheiro');
+  const [tag, setTag] = useState('lazer');
   const dispatch = useDispatch();
   const [expense, setExpense] = useState([]);
   const [cot, setCot] = useState([]);
 
-  const handleExpensesAdd = () => {
-    dispatch(submitExpense(20));
-    dispatch(submitTable('teste'));
+  const handleExpensesAdd = (e) => {
+    e.preventDefault();
+    dispatch(submitExpense(Number(valor)));
+    dispatch(submitTable([valor, moeda, metodo, tag, descricao]));
   };
 
   useEffect(() => {
@@ -26,41 +27,48 @@ const Bar = () => {
   return (
 
     <div className={ styles.container }>
+      <form onSubmit={ (e) => handleExpensesAdd(e) }>
+        Valor:
+        {' '}
+        <input value={ valor } onChange={ (e) => setValor(e.target.value) } required />
+        Moeda:
+        {' '}
 
-      Valor:
-      {' '}
-      <input />
-      Moeda:
-      {' '}
-
-      <select name="moeda">
-        {
-          Object.values(cot).map((val) => <option key={ val.code } value={ val.code }>{val.code}</option>)
-        }
-      </select>
-      Método de
-      {' '}
-      <br />
-      {' '}
-      pagamento:
-      {' '}
-      <select name="mPagamento">
-        <option value="dinheiro">Dinheiro</option>
-        <option value="cart_credito">Cartão de Crédito</option>
-        <option value="cart_debito">Cartão de Débito</option>
-      </select>
-      Tag:
-      <select name="tag">
-        <option value="lazer">Lazer</option>
-        <option value="alimentacao">Alimentação</option>
-        <option value="trabalho">Trabalho</option>
-        <option value="transporte">Transporte</option>
-        <option value="saude">Saúde</option>
-      </select>
-      Descrição:
-      {' '}
-      <input />
-      <button type="button" className={ styles.button } onClick={ () => handleExpensesAdd() }>Adicionar despesa</button>
+        <select name="moeda" onChange={ (e) => setMoeda(e.target.value) }>
+          {
+            Object.values(cot).map((val) => <option key={ val.code } value={ val.code }>{val.code}</option>)
+          }
+        </select>
+        Método de
+        {' '}
+        <br />
+        {' '}
+        pagamento:
+        {' '}
+        <select name="mPagamento" onChange={ (e) => setMetodo(e.target.value) }>
+          <option value="dinheiro">Dinheiro</option>
+          <option value="cart_credito">Cartão de Crédito</option>
+          <option value="cart_debito">Cartão de Débito</option>
+        </select>
+        Tag:
+        <select name="tag" onChange={ (e) => setTag(e.target.value) }>
+          <option value="lazer">Lazer</option>
+          <option value="alimentacao">Alimentação</option>
+          <option value="trabalho">Trabalho</option>
+          <option value="transporte">Transporte</option>
+          <option value="saude">Saúde</option>
+        </select>
+        Descrição:
+        {' '}
+        <input
+          value={ descricao }
+          onChange={
+            (e) => setDescricao(e.target.value)
+          }
+          required
+        />
+        <button type="submit" className={ styles.button }>Adicionar despesa</button>
+      </form>
     </div>
   );
 };
--- src/components/table.js
+++ src/components/table.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import styles from '../css/table.module.css'
+import styles from '../css/table.module.css';
+
 const Table = () => {
   const [date, setDate] = useState([]);
   const result = useSelector((state) => state.Wallet);
@@ -24,6 +25,18 @@ const Table = () => {
           <th>Editar/Excluir</th>
         </tr>
       </thead>
+      <tbody>
+        {date.map((t, key) => (<tr key={ key }>
+          <td>{t[4]}</td>
+          <td>{t[3]}</td>
+          <td>{t[2]}</td>
+          <td>R$ {Number(t[0]).toFixed(2)}</td>
+          <td>{t[1]}</td>
+          <td>{t[0]}</td>
+          <td>{t[0]}</td>
+        </tr>))}
+
+      </tbody>
     </table>
 
   );
